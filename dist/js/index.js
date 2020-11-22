df28df4c12fef3621b130372acb12c96bdcd1afa
chemingqiang <2247013784@qq.com>
2020-11-22 18:29:18 +0800

    样式首页11.12

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,4 +2,9 @@
 
 document.querySelector('h1').onclick = function () {
   alert(123);
-};
+}; //下拉列表
+// $(document).ready(function(){
+//   $(".b").click(function(){
+//     $(".section").slideDown("slow");
+//   });
+// });
